b1fe2f8fcedb12036c11288dab5412af0646c245
imoikhnii-east <i.mokhnii@eastoner.net>
2021-08-25 17:58:56 +0300

    Initial

---
+++ Git.Learn/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Git.Learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test git stuff");
+            Console.ReadLine();
+        }
+    }
+}
